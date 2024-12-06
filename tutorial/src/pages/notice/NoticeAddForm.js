e2bce4c7374816bb9b5e4ac5086b4695e3d0f4e8
최은영 <dev.choiey@gmail.com>
2024-12-06 14:41:16 +0900

    Update NoticeAddForm.js

--- tutorial/src/pages/notice/NoticeAddForm.js
+++ tutorial/src/pages/notice/NoticeAddForm.js
@@ -3,5 +3,18 @@ import { AuthContext } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const NoticeForm = () => {
-    
+    const { springBootAxiosInstance } = useContext(AuthContext);
+    const [form, setForm] = useState({ notTitle:'', notContent: ''});
+    const navigate=useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value});
+    };
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+    }
+
 }
